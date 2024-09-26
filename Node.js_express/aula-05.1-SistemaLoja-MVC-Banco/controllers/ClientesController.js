e7d3a532e68b3e5b2496d0b4588e029896b44c61
DSM2 <fatec-dsm2@academico.intra>
2024-09-25 22:46:54 -0300

    Versão 0.1.1

---
+++ Node.js_express/aula-05.1-SistemaLoja-MVC-Banco/controllers/ClientesController.js
@@ -0,0 +1,58 @@
+import express from 'express'
+const router = express.Router()
+
+// Importando o model de Cliente
+
+import Cliente from '../models/Cliente.js'
+
+// ROTA CLIENTES
+router.get("/clientes", function(req,res){
+    
+    Cliente.findAll().then(clientes => {
+
+        res.render("clientes", {
+            clientes: clientes
+        })
+
+    })
+
+    
+})
+
+// ROTA DE CADASTRO DE CLIENTES
+
+router.post("/clientes/new", (req, res) =>{
+
+    const nome =req.body.nome
+    const cpf = req.body.cpf
+    const endereco = req.body.endereco
+    Cliente.create({
+
+        nome : nome,
+        cpf : cpf,
+        endereco : endereco
+            
+    }).then(() => {
+
+        res.redirect("/clientes")
+
+    })
+
+})
+
+
+
+router.get("/clientes/delete/:id", (req, res) => {
+    const id = req.params.id
+
+    Cliente.destroy({
+        where:{
+            id:id
+        }
+    }),then(() => {
+        res.redirect("/clientes")
+    }).catch(error => {
+        console.log(error);
+    })
+})
+export default router
